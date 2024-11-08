cfaac5c26f699ac71490fd7cd4f796795a85251f
abhisheklodha001 <abhishek.lodha.jobsdoor360@gmail.com>
2024-11-08 22:45:59 +0530

    variable in cpp

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    int num = 5;
+    // num is variable that hold the value 5 
+    // variable is a memory location or name of memory location that hold some kind of data
+    return 0;
+}
