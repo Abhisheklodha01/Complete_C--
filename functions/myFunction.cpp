f7a481eef8efc4aefa3091f0529686fed138c5de
abhisheklodha001 <abhishek.lodha.jobsdoor360@gmail.com>
2024-11-09 18:25:45 +0530

    functions in cpp

--- functions/myFunction.cpp
+++ functions/myFunction.cpp
@@ -1,8 +1,39 @@
-#include<iostream>
-using namespace std; 
+#include <iostream>
+using namespace std;
+
+/*
+       function
+ returnType functionName(parameter)
+ {
+    body of function
+ }
+
+     returnType
+     void, int, double, string, float
+
+     parameter ---> it is an optional to give in function
+     fucntionName ---> user defined name but not predefined keywords
+     body ---> set of task that function do
+
+*/
+
+int checkTemprature(int tempratureValue)
+{
+    return tempratureValue;
+}
+
+// declaration of function
+void serverChai(int cups); // we can defined function later on
 
 int main()
 {
-    
+    int ans = checkTemprature(32);
+    cout << ans<<endl;;
+    serverChai(5);
     return 0;
+}
+
+void serverChai(int cups)
+{
+    cout << cups << endl;
 }
