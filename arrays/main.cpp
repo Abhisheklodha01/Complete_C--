0a0dee7a453ccba73d5c05375f9fc7d1189a7f20
abhisheklodha001 <abhishek.lodha.jobsdoor360@gmail.com>
2024-11-10 17:25:22 +0530

    arrays, pointers in cpp

---
+++ arrays/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // array
+    int chaiTemprature[5] = {85, 90, 88, 92, 89};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << chaiTemprature[i] << " ";
+    }
+    return 0;
+}
